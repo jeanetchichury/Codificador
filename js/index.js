d5322db53cfa2589bcdb5fccc0225c2f34a31457
jeanetchichury <jean.etchichury@hotmail.com>
2021-08-30 13:29:57 -0300

    Commit inicial

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// var str = document.querySelector('p').textContent
+// var coded = [str.charCodeAt(0), str.charCodeAt(1), str.charCodeAt(2), str.charCodeAt(3), str.charCodeAt(4), str.charCodeAt(5), str.charCodeAt(6), str.charCodeAt(7)]
+// var decoded = String.fromCharCode(coded[0], coded[1], coded[2], coded[3], coded[4], coded[5], coded[6], coded[7])
+
+
+var coded = ""
+function toCifra(msg, num){
+    let asc
+    for (i=0; i<msg.length; i++){
+        if (msg[i] == " ")
+        coded += " "
+        else {
+            if (msg.charCodeAt(i)+num>122)
+            asc = msg.charCodeAt(i)+num-26
+            else
+            asc = msg.charCodeAt(i)+num
+            coded += String.fromCharCode(asc)
+        }
+    }
+return coded
+}
+var decoded = ""
+function toNormal(msg, num){
+    let letra
+    for (i=0; i<msg.length; i++){
+        if (msg[i] == " ")
+        coded += " "
+        else {
+            if (msg.charCodeAt(i)-num<97)
+            letra = msg.charCodeAt(i)-num+26
+            else
+            letra = msg.charCodeAt(i)-num
+            coded += String.fromCharCode(letra)
+        }
+    }
+return coded
+}
+// console.log(document.querySelector('#tipo').value)
